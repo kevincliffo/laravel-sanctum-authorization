53facfadae3e24e39d00cb17db3cfd08d571c3b9
Kevin Ochieng <kevincliffo@gmail.com>
2021-08-20 23:30:26 +0300

    Re-added email to users controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,13 +14,13 @@ class UserController extends Controller
         $fields = $request->validate([
             'name'=>'required|string',
             'phone_number'=>'required|digits:10',
-            //'email'=>'required|string|unique:users,email',
+            'email'=>'required|string|unique:users,email',
             'password'=>'required|string|confirmed'
         ]);
 
         $user = User::create([
             'name' => $fields['name'],
-            //'email' => $fields['email'],
+            'email' => $fields['email'],
             'phone_number' => $fields['phone_number'],
             'password' => bcrypt($fields['password'])
         ]);
@@ -51,12 +51,18 @@ class UserController extends Controller
         }
         
         $token = $user->createToken('myapptoken')->plainTextToken;
-        $response = [
-            'user' => $user,
-            'token' => $token
-        ];
+        // $response = [
+        //     'user' => $user,
+        //     'token' => $token
+        // ];
 
-        return response($response, 201);
+        $response = response()->json([
+            'user' => $user,
+            'token' => $token,
+            'statusCode' => 201
+        ]);
+        //return response($response, 201);
+        return response($response);
     }    
 
     public function logout(Request $request)
